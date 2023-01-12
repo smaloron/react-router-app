11d05371680d8f76a2128cc2a18d810e648f042f
seb <contact@smaloron.eu>
2023-01-12 10:43:58 +0100

    mise en place du projet

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { RouterProvider, createBrowserRouter, Router } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Products from './pages/Products';
-import Navbar from './components/Navbar';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 
 // Définition des routes
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/contact', element: <Contact /> },
-  { path: '/produits/:id', element: <Products /> },
+  { path: '/', element: <TaskList /> },
+  { path: '/details/:id', element: <TaskDetails /> },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '../components/Navbar';
-export default function Contact () {
-    return (
-        <div>
-            <Navbar />
-            <h1>Contact</h1>
-        </div>
-    );
-}
